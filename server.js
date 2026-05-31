4fce24841d4c34e642e173582f666cb9b1b54199
Gary William Mendel <garywmendel@gmail.com>
2026-05-30 21:04:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-
+ 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app  = express();
 const PORT = process.env.PORT || 3000;
